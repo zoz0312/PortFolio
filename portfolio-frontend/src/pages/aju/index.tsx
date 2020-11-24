e60b706cc0d52fa1571d7c130ff437ee0c847ef0
zoz0312 <zoz0312@naver.com>
2020-11-24 17:19:56 +0900

    craco (path manage) add

--- portfolio-frontend/src/pages/aju/index.tsx
+++ portfolio-frontend/src/pages/aju/index.tsx
@@ -1,9 +1,11 @@
 import { Route, RouteComponentProps } from "react-router-dom"
 import Home from './aju.Home';
 import About from './aju.About';
+import { JsxElement } from "typescript";
 
-const AJu: React.FC<RouteComponentProps> = ({ match }: RouteComponentProps) => {
-  console.log('props', match)
+const AJu: React.FC<RouteComponentProps> = (
+  { match }: RouteComponentProps,
+): JSX.Element => {
   return (
     <>
       <Route exact path={`${match.path}`} component={Home} />
--- portfolio-frontend/src/pages/index.tsx
+++ portfolio-frontend/src/pages/index.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import Main from './Main';
-import AJu from './aju/index';
+import Home from '@pages/Home';
+import Main from '@pages/Main';
+import AJu from '@pages/aju/index';
 
 const Root: React.FC = () => (
   // TODO 루트 라우터를 반환한다
