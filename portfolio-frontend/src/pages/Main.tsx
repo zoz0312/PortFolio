d40b8392b703e5ec40a7969f7eb6026955904e5d
zoz0312 <zoz0312@naver.com>
2020-11-24 18:09:18 +0900

    default css setting

--- portfolio-frontend/src/pages/Main.tsx
+++ portfolio-frontend/src/pages/Main.tsx
@@ -1,24 +1,30 @@
+import Avatar from 'antd/lib/avatar/avatar';
 import React from 'react';
-import logo from '../logo.svg';
+import { Link } from 'react-router-dom';
+import { UserOutlined } from '@ant-design/icons';
+import './Main.scss'
 
 function Main() {
   return (
-    <>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="Main-heade">
+        Header
       </header>
-    </>
+      <div className="flex-container">
+        <Link to="/aju" className="flex-item">
+          <div>
+            <Avatar size={128} icon={<UserOutlined />} />
+          </div>
+          AJu
+        </Link>
+        <Link to="/euri" className="flex-item">
+          <div>
+            <Avatar size={128} icon={<UserOutlined />} />
+          </div>
+          Euri
+        </Link>
+      </div>
+    </div>
   );
 }
 
